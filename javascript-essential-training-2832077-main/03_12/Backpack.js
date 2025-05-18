04118381b21a5c37cfd92a506ef6d5e786c800ce
nsyafantrin <nsyafantrin@gmail.com>
2025-05-18 07:57:23 +0700

    update

--- javascript-essential-training-2832077-main/03_12/Backpack.js
+++ javascript-essential-training-2832077-main/03_12/Backpack.js
@@ -6,7 +6,7 @@ class expression : const Name = class {}
 
 */
 
-class Backpack {
+ class Backpack {
     constructor(
         // Defines parameters:
         name,
@@ -38,4 +38,74 @@ class Backpack {
     }
 }
 
-export default Backpack;
+
+// export default Backpack;
+
+
+// EXTENDING CLASSES
+
+class HikingBackpack extends Backpack {
+    constructor (
+        // define propertises
+        name,
+        volume,
+        color,
+        pocketNum,
+        strapLengthL,
+        strapLengthR,
+        lidOpen,
+        hydrationCapacity
+    ) {
+        // initialize the parent class properties
+        super(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen);
+        // new property specific to HikingBacpkack
+        this.hydrationCapacity = hydrationCapacity;
+    }
+}
+
+export default HikingBackpack;
+
+/*
+function Backpack (
+    name,
+    volume,
+    color,
+    pocketNum,
+    strapLengthL,
+    strapLengthR,
+    lidOpen
+) {
+    this.name = name;
+    this.volume = volume;
+    this.color = color;
+    this.pocketNum = pocketNum;
+    this.strapLength = {
+        left: strapLengthL,
+        right: strapLengthR,
+    };
+    this.lidOpen = lidOpen;
+    this.toggleLid = function 
+    (lidStatus) {
+        this.lidOpen = lidStatus;
+    };
+    this.newStrapLength = function
+    (lengthLeft, lengthRight) {
+        this.strapLength.left = lengthLeft;
+        this.strapLength.right = lengthRight;
+    };
+}
+
+const everydayPack = new Backpack (
+    "Everyday Backpack",
+    30,
+    "grey",
+    15,
+    26,
+    26,
+    false
+);
+
+console.log(everydayPack);
+console.log(Backpack);
+
+*/
